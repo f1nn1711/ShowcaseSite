84aae4d1ed75011794c6646435b094b5e01a0368
Finn Hunt <hunt.finn@gmail.com>
2021-09-30 00:28:11 +0100

    added features
    added car model to sketch, added plane added car class

--- static/script.js
+++ static/script.js
@@ -1,7 +1,59 @@
+var parentDiv = document.getElementById('sketchDiv');
+var divWidth = parentDiv.offsetWidth;
+var divHeight = parentDiv.offsetHeight;
+
+var carModel;
+
+class Car {
+    constructor(x,y,z,angle) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.angle = angle;
+
+        this.model = carModel;
+    }
+}
+
 function setup() {
-    createCanvas(400, 400);
+    console.log(divWidth)
+    console.log(divHeight)
+    var canvas = createCanvas(divWidth, divHeight, WEBGL);
+    canvas.parent('#sketchDiv');
+}
+
+function preload() {
+    carModel = loadModel('car_model.obj', true)
 }
 
 function draw() {
-    background(220);
+    background(230);
+
+    camera(
+        x=0,
+        y=0,
+        z=(height/2)/tan(PI/6),
+        centerX=0,
+        centerY=0,
+        centerX=0,
+        upX=0,
+        upY=1,
+        upZ=0
+    );
+
+    normalMaterial();
+    push();
+    rotateX(2.2);
+    rotateY(frameCount * 0.01);
+    translate(0,50,0);
+    model(carModel);
+    pop();
+
+    push();
+    rotateX(0.64);
+    plane(200);
+    pop();
+
+    
+    
 }
